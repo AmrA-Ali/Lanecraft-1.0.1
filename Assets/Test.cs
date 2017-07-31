1710a218f537c07e2d26acdd199ad71670e1e27e
Azzazy <msa.azzazy@aucegypt.edu>
2017-07-31 15:16:56 +0200

    Testing Slot back-end, BuySlot working

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(test);
+    }
+
+    private static void test()
+    {
+        var length = 1;
+        LC.Online.Online.BuySlot(length, d =>
+        {
+            Debug.Log("Test.BuySlotCallback: " + (bool) d["status"]);
+//            Debug.Log(d["slot"]);
+        });
+    }
+}
